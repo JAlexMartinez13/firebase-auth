53c32a2d102177cae2d88ccc25e604578bf6cd6d
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-02-16 16:06:20 -0500

    added logout in welcome screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from './scenes/Login';
 import Signup from './scenes/Signup';
@@ -7,13 +7,17 @@ import Welcome from './scenes/Welcome';
 
 function App() {
   const [user, setUser] = useState();
+
+  useEffect(()=> {
+    console.log('Here is my useEffect')
+  },
+  [])
   
   return (
    <Routes>
-     <Route path='/login' element={<Login setUser={
-       setUser}/>}/>
+     <Route path='/login' element={<Login setUser={setUser} user={user}/>}/>
      <Route path='/signup' element={<Signup setUser={setUser} />}/>
-     <Route path='/' element={user ? <Welcome user={user}/> : <Login setUser={
+     <Route path='/' element={user ? <Welcome user={user} setUser={setUser}/> : <Login setUser={
        setUser}/>}/>
    </Routes>
   );
--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,16 +1,35 @@
+import { useNavigate } from "react-router-dom"
+import { getAuth, signOut } from "firebase/auth"
 
 
-export default function Welcome( {user} ){
-      //email, displayName, photoURL
+export default function Welcome( {user, setUser} ){
+     console.log(user) //email, displayName, photoURL
+
+     const auth = getAuth()
+     const navigate = useNavigate()
+
+     const handleLogout = () => {
+         signOut(auth)
+         .then(()=>{
+             navigate('/login')
+             localStorage.clear()
+            })
+        .catch(err=> console.log(err))
+    }
+        
+      
     return (
     <>
         <h1>Welcome</h1>
-        <h2>{user.displayName || user.email}</h2>
-        {user.photoURL && <img src={user.photoURL} 
+        <h2>{localStorage.getItem('displayName') || user.email}</h2>
+        {localStorage.getItem('profilePhoto')
+        && <img src={localStorage.getItem('profilePhoto')} 
         alt="Profile pic of logged-in user"/>}
+        <button onClick={handleLogout}>Sign out</button>
     </>
     )
 }
+        
 
 
  
