71098106496701cdfe185482168d5924c9409df9
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-02-16 10:29:23 -0500

    Added login, welcome, sign-up

---
+++ src/scenes/Login.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+export default function Login(){
+    return (
+        <>
+        <h1>Login</h1>
+        <p>Not a user?<Link to='/signup'>Sign up</Link></p>
+        </>
+    )
+}
